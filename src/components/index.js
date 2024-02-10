97ef50578ded40f575bde64899677aee4a93789c
Gautam5722 <[200101120073@cutm.ac.in]>
2024-02-10 17:54:02 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import InputBox from "./InputBox";
+
+export {InputBox}
+
+
+// or bhi agar component rahega to ushe import kara lenge or ishi 
+// export me comma , laga ke export kar denge  export{InputBox,searchComponent,convertcomponent } like that
